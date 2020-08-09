b3dbc9094e781bb3444515fe2b4a770563afc247
jgaik <jakub.gaik@gmail.com>
2020-08-09 16:47:25 +0200

    gui layout init

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import tkinter.filedialog as fdiag
 import tkinter as tk
 from tkinter import ttk
 import ttkwidgets
-#import pyperclip
+# import pyperclip
 import downloader as dl
 import re
 import threading as th
 import concurrent.futures as concurr
 import queue as q
 import os
+
 
 class App:
     TREE_MEDIA_HEIGHT_MIN = 1
@@ -28,13 +29,15 @@ class App:
         DESTINATION = "#4"
 
         @classmethod
-        def to_columns(cls):
+        def to_columns(cls, all=False):
             attr = cls.__dict__
             out = []
             for a in attr:
                 if not callable(getattr(cls, a)):
                     if not a.startswith("__"):
-                        if attr[a] != "#0":
+                        if all:
+                            out.append(attr[a])
+                        elif attr[a] != "#0":
                             out.append(attr[a])
             return tuple(out)
 
@@ -42,33 +45,48 @@ class App:
         self.master = master
         self._flag_update = False
         self._queue_media = q.Queue()
+        self._flag_download = False
 
         self.map_media = {}
 
-        self.button_clipboard = ttk.Button(
-            self.master, text="Add from clipboard", command=th.Thread(target=self.event_clipboard).start)
-        
-        self.progress_info = ttk.Progressbasr(self.master)
+        self.frame_add = ttk.Frame(self.master)
+        self.frame_dir = ttk.Frame(self.master)
+        self.frame_tree = ttk.Frame(self.master)
 
+        self.style_progress = ttk.Style(self.master)
+        self.style_progress.layout('Horizontal.TProgressbar',
+                                   [('Horizontal.Progressbar.trough', {
+                                       'children': [('Horizontal.Progressbar.pbar', {'side': 'left', 'sticky': 'ns'})],
+                                       'sticky': 'nswe'}),
+                                       ('Horizontal.Profressbar.label', {'sticky': ''})])
+
+        self.button_clipboard = ttk.Button(
+            self.frame_add, text="Add from clipboard", command=self.event_clipboard)
+        self.button_download = ttk.Button(
+            self.frame_add, text="Download selected", command=self.event_download)
         self.var_check_audio = tk.StringVar()
         self.var_check_audio.set(self.Format.VIDEO)
-        self.check_audio = ttk.Checkbutton(self.master, text="Audio only (mp3)",
-            onvalue=self.Format.AUDIO, offvalue=self.Format.VIDEO, variable=self.var_check_audio, command=self.event_check_audio)
+        self.check_audio = ttk.Checkbutton(self.frame_add, text="Audio only (mp3)",
+                                           onvalue=self.Format.AUDIO, offvalue=self.Format.VIDEO, variable=self.var_check_audio, command=self.event_check_audio)
 
         self.var_dir_default = tk.StringVar()
-        self.var_dir_default.set(os.getcwd())
-        label_dir_default = ttk.Label(self.master, text="Default directory")
-        self.entry_dir_default = ttk.Entry(self.master, textvariable=self.var_dir_default)
+        self.var_dir_default.set(os.path.expanduser("~"))
+        label_dir_default = ttk.Label(self.frame_dir, text="Default directory")
+        self.entry_dir_default = ttk.Entry(
+            self.frame_dir, textvariable=self.var_dir_default, state=tk.DISABLED, width=len(
+                self.var_dir_default.get()))
+        self.entry_dir_default.bind("<Double-1>", self.event_dir_default)
 
         self.var_check_subdir = tk.BooleanVar()
         self.var_check_subdir.set(True)
-        self.check_subdir = tk.Checkbutton(self.master, text="Playlists in subfolders", 
-            onvalue=True, offvalue=False, variable=self.var_check_subdir)
+        self.check_subdir = ttk.Checkbutton(self.frame_dir, text="Playlists in subfolders",
+                                            onvalue=True, offvalue=False, variable=self.var_check_subdir)
 
-        self.button_dir_default = ttk.Button(self.master, text="Change default", command=self.event_dir_default)
+        self.button_dir_default = ttk.Button(
+            self.frame_dir, text="Change default", command=self.event_dir_default)
 
         self.tree_media = ttkwidgets.CheckboxTreeview(
-            self.master, height=self.TREE_MEDIA_HEIGHT_MAX, columns=self.Column.to_columns())
+            self.frame_tree, height=self.TREE_MEDIA_HEIGHT_MAX, columns=self.Column.to_columns())
         self.tree_media.bind("<Button-1>", self.event_tree_click)
         self.tree_media.bind("<Double-1>", self.event_tree_doubleclick)
 
@@ -78,23 +96,60 @@ class App:
         self.tree_media.heading(self.Column.FORMAT, text="Format")
         self.tree_media.heading(self.Column.DESTINATION, text="Destination")
 
-        self.tree_media.column(self.Column.FORMAT, anchor="center")
+        self.tree_media.column(self.Column.STATUS, width=150)
+        self.tree_media.column(self.Column.FORMAT, anchor="center", width=100)
+        self.tree_media.column(self.Column.TITLE, width=350)
 
-        self.button_clipboard.pack()
-        self.check_audio.pack()
-        self.tree_media.pack()
-        label_dir_default.pack()
-        self.entry_dir_default.pack()
-        self.check_subdir.pack()
-        self.button_dir_default.pack()
+        l = sum([self.tree_media.column(c, option='width')
+                 for c in self.Column.to_columns(True)])
+        self.progress_info = ttk.Progressbar(self.master, length=l)
+
+        self.frame_add.grid(row=0, column=0)
+        self.frame_tree.grid(row=1, column=0)
+        self.frame_dir.grid(row=0, column=1, rowspan=2)
+
+        self.button_clipboard.grid(row=0, column=0)
+        self.check_audio.grid(row=0, column=1)
+        self.button_download.grid(row=0, column=2)
+
+        self.tree_media.grid()
+
+        label_dir_default.grid(row=0, column=0)
+        self.entry_dir_default.grid(row=1, column=0)
+        self.check_subdir.grid(row=2, column=0)
+        self.button_dir_default.grid(row=3, column=0)
+        self.progress_info.grid(row=2, column=0)
+
+    def progress_start(self, max, text):
+        self.progress_info['maximum'] = max
+        self.style_progress.configure(
+            'Horizontal.TProgressbar', text=text)
+
+    def progress_update(self):
+        self.progress_info['value'] += 1
+
+    def progress_reset(self):
+        self.progress_info['value'] = 0
+        self.style_progress.configure(
+            'Horizontal.TProgressbar', text='')
 
     def event_clipboard(self):
+        if not self._flag_update and not self._flag_download:
+            thread = th.Thread(target=self.thread_clipboard)
+            thread.start()
+
+    def event_download(self):
+        pass
+
+    def thread_clipboard(self):
         self._flag_update = True
         th.Thread(target=self.update_tree).start()
-        #urls = pyperclip.paste()
-        urls = 'https://www.youtube.com/watch?v=hS5CfP8n_js, https://www.youtube.com/watch?v=0MW0mDZysxc, https://www.youtube.com/watch?v=xxSTOGFoDeg&list=PLrryvZ-gdCErP01mLIHNidblxMovnockz, https://www.youtube.com/watch?v=Owx3gcvark8'
+        # urls = pyperclip.paste()
+        urls = 'https://www.youtube.com/watch?v=hS5CfP8n_js, https://www.youtube.com/watch?v=0MW0mDZysxc, https://www.youtube.com/playlist?list=PLiM-0FYH7IQ-awx_UzfJd6XwiZP--dnli, https://www.youtube.com/watch?v=Owx3gcvark8'
+        urls = re.split(r'[\s,]+', urls)
+        self.progress_start(len(urls), 'Retrieving info..')
         with concurr.ThreadPoolExecutor() as executor:
-            executor.map(self.get_media_info, re.split(r'[\s,]+', urls))
+            executor.map(self.get_media_info, urls)
         self._flag_update = False
 
     def get_media_info(self, url):
@@ -109,24 +164,24 @@ class App:
 
         while self._flag_update:
             media_new = self._queue_media.get()
-        
+
             dest = ""
             if media_new.type == dl.Type.SINGLE:
                 if media_new.media.status == dl.Status.OK:
-                    dest = os.sep.join(["~", media_new.media.title]) 
-                id = self.tree_media.insert('', 'end', text=media_new.media.url, 
+                    dest = os.sep.join(["~", media_new.media.title])
+                id = self.tree_media.insert('', 'end', text=media_new.media.url,
                                             values=(
-                                                    media_new.media.status.value, 
-                                                    media_new.media.title, 
-                                                    self.var_check_audio.get(),
-                                                    dest),
+                                                media_new.media.status.value,
+                                                media_new.media.title,
+                                                self.var_check_audio.get(),
+                                                dest),
                                             tags=statuscheck(media_new.media))
             if media_new.type == dl.Type.PLAYLIST:
                 if self.var_check_subdir.get():
                     dest = os.sep.join(["~", media_new.media.title])
                 else:
-                    dest = "~" + os.sep
-                id = self.tree_media.insert('', 'end', text=media_new.media.url, 
+                    dest = "~"
+                id = self.tree_media.insert('', 'end', text=media_new.media.url,
                                             values=(
                                                 f"Extracted {len(media_new.media_list)} of {media_new.count}",
                                                 media_new.media.title,
@@ -135,15 +190,17 @@ class App:
                                             tags='checked')
                 for m in media_new.media_list:
                     d = os.sep.join([dest, m.title])
-                    self.tree_media.insert(id, 'end', text=m.url, 
-                                            iid="_".join([id,str(m.idx)]),
-                                            values=(
-                                                m.status.value, 
-                                                m.title, 
-                                                self.var_check_audio.get(),
-                                                d), 
-                                            tags=statuscheck(m))
+                    self.tree_media.insert(id, 'end', text=m.url,
+                                           iid="_".join([id, str(m.idx)]),
+                                           values=(
+                                               m.status.value,
+                                               m.title,
+                                               self.var_check_audio.get(),
+                                               d),
+                                           tags=statuscheck(m))
             self.map_media[id] = media_new
+            self.progress_update()
+        self.progress_reset()
 
     def event_tree_click(self, event):
         x, y, widget = event.x, event.y, event.widget
@@ -167,32 +224,40 @@ class App:
 
         if self.tree_media.identify_column(x) == self.Column.FORMAT:
             if val == self.Format.AUDIO:
-                self.tree_media.set(item, self.Column.FORMAT, self.Format.VIDEO)
+                self.tree_media.set(
+                    item, self.Column.FORMAT, self.Format.VIDEO)
                 if parent:
-                    vals_parent = self.tree_media.item(parent, option="values")
                     if all([self.tree_media.set(v, self.Column.FORMAT) == self.Format.VIDEO for v in self.tree_media.get_children(parent)]):
-                        self.tree_media.set(parent, self.Column.FORMAT, self.Format.VIDEO)
+                        self.tree_media.set(
+                            parent, self.Column.FORMAT, self.Format.VIDEO)
                     else:
-                        self.tree_media.set(parent, self.Column.FORMAT, self.Format.BOTH)
+                        self.tree_media.set(
+                            parent, self.Column.FORMAT, self.Format.BOTH)
                 else:
                     for child in children:
-                        self.tree_media.set(child, self.Column.FORMAT, self.Format.VIDEO)
+                        self.tree_media.set(
+                            child, self.Column.FORMAT, self.Format.VIDEO)
             else:
-                self.tree_media.set(item, self.Column.FORMAT, self.Format.AUDIO)
+                self.tree_media.set(
+                    item, self.Column.FORMAT, self.Format.AUDIO)
                 if parent:
-                    vals_parent = self.tree_media.item(parent, option="values")
                     if all([self.tree_media.set(v, self.Column.FORMAT) == self.Format.AUDIO for v in self.tree_media.get_children(parent)]):
-                        self.tree_media.set(parent, self.Column.FORMAT, self.Format.AUDIO)
+                        self.tree_media.set(
+                            parent, self.Column.FORMAT, self.Format.AUDIO)
                     else:
-                        self.tree_media.set(parent, self.Column.FORMAT, self.Format.BOTH)
+                        self.tree_media.set(
+                            parent, self.Column.FORMAT, self.Format.BOTH)
                 else:
                     for child in children:
-                        self.tree_media.set(child, self.Column.FORMAT, self.Format.AUDIO)
+                        self.tree_media.set(
+                            child, self.Column.FORMAT, self.Format.AUDIO)
             if val == self.Format.BOTH:
-                self.tree_media.set(item, self.Column.FORMAT, self.Format.VIDEO)
+                self.tree_media.set(
+                    item, self.Column.FORMAT, self.Format.VIDEO)
                 for child in children:
-                    self.tree_media.set(child, self.Column.FORMAT, self.Format.VIDEO)
-            
+                    self.tree_media.set(
+                        child, self.Column.FORMAT, self.Format.VIDEO)
+
             if all([self.tree_media.set(i, self.Column.FORMAT) == self.Format.AUDIO for i in self.tree_media.get_children()]):
                 self.var_check_audio.set(self.Format.AUDIO)
             else:
@@ -216,50 +281,61 @@ class App:
                     dir = dir[0:prev]
             else:
                 dest = self.tree_media.set(parent, self.Column.DESTINATION)
-                if not os.path.isdir(dir:=dest.replace('~', self.var_dir_default.get())):
+                if not os.path.isdir(dir := dest.replace('~', self.var_dir_default.get())):
                     prev = dir.rfind(os.sep)
                     dir = dir[0:prev]
             dir_new = fdiag.askdirectory(initialdir=dir, mustexist=True)
-            dest_new = dir_new.replace(self.var_dir_default.get(),'~')
             if dir_new:
+                dest_new = dir_new.replace(self.var_dir_default.get(), '~')
                 if not parent:
                     if not children:
-                        self.tree_media.set(item, self.Column.DESTINATION, 
-                            os.sep.join([dest_new, self.tree_media.set(item, self.Column.TITLE)]))
+                        self.tree_media.set(item, self.Column.DESTINATION,
+                                            os.sep.join([dest_new, self.tree_media.set(item, self.Column.TITLE)]))
                     else:
-                        self.tree_media.set(item, self.Column.DESTINATION, dest_new)
+                        self.tree_media.set(
+                            item, self.Column.DESTINATION, dest_new)
                         for child in children:
-                            self.tree_media.set(child, self.Column.DESTINATION, 
-                                os.sep.join([dest_new, self.tree_media.set(child, self.Column.TITLE)]))
+                            self.tree_media.set(child, self.Column.DESTINATION,
+                                                os.sep.join([dest_new, self.tree_media.set(child, self.Column.TITLE)]))
                 else:
-                    self.tree_media.set(parent, self.Column.DESTINATION, dest_new)
+                    self.tree_media.set(
+                        parent, self.Column.DESTINATION, dest_new)
                     for child in self.tree_media.get_children(parent):
-                        self.tree_media.set(child, self.Column.DESTINATION, 
-                            os.sep.join([dest_new, self.tree_media.set(child, self.Column.TITLE)]))
+                        self.tree_media.set(child, self.Column.DESTINATION,
+                                            os.sep.join([dest_new, self.tree_media.set(child, self.Column.TITLE)]))
 
         if self.tree_media.identify_column(x) == self.Column.TITLE:
             title_old = self.tree_media.set(item, self.Column.TITLE)
-            title_new = sdiag.askstring('Edit title', 'Enter new title', initialvalue=title_old)
+            title_new = sdiag.askstring(
+                'Edit title', 'Enter new title', initialvalue=title_old)
             if title_new:
                 self.tree_media.set(item, self.Column.TITLE, title_new)
-                self.tree_media.set(item, self.Column.DESTINATION, 
-                    self.tree_media.set(item, self.Column.DESTINATION).replace(title_old, title_new))
+                self.tree_media.set(item, self.Column.DESTINATION,
+                                    self.tree_media.set(item, self.Column.DESTINATION).replace(title_old, title_new))
                 if not parent:
                     if children:
                         for child in children:
-                            self.tree_media.set(child, self.Column.DESTINATION, 
-                                self.tree_media.set(child, self.Column.DESTINATION).replace(title_old, title_new))
+                            self.tree_media.set(child, self.Column.DESTINATION,
+                                                self.tree_media.set(child, self.Column.DESTINATION).replace(title_old, title_new))
+
+        if not self.tree_media.identify_column(x) == self.Column.STATUS:
+            self.tree_media.item(
+                item, open=not self.tree_media.item(item, 'open'))
 
     def event_check_audio(self):
         for child in self.tree_media.get_children():
-            self.tree_media.set(child, self.Column.FORMAT, self.var_check_audio.get())
+            self.tree_media.set(child, self.Column.FORMAT,
+                                self.var_check_audio.get())
             for subchild in self.tree_media.get_children(child):
-                self.tree_media.set(subchild, self.Column.FORMAT, self.var_check_audio.get())
-    
-    def event_dir_default(self):
-        dir = fdiag.askdirectory(initialdir=self.var_dir_default.get(), mustexist=True)
+                self.tree_media.set(
+                    subchild, self.Column.FORMAT, self.var_check_audio.get())
+
+    def event_dir_default(self, event):
+        dir = fdiag.askdirectory(
+            initialdir=self.var_dir_default.get(), mustexist=True)
         if dir:
             self.var_dir_default.set(dir)
+
 
 if __name__ == '__main__':
     root = tk.Tk()
