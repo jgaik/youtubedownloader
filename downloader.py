bd33cb58fecd804657012aad448061f9bd56463e
Jakub Gaik <52374575+jgaik@users.noreply.github.com>
2020-08-19 16:45:27 +0200

    changed downloader to separate medias

--- downloader.py
+++ downloader.py
@@ -1,17 +1,41 @@
 import youtube_dl as yt
 import threading as th
 import os
+from collections.abc import Sequence
 
+def extractMedia(url):
+    opts = {
+        'format': 'bestaudio/best',
+        'quiet': True,
+        'ignoreerrors': True,
+    }
+    with yt.YoutubeDL(opts) as ydl:
+        data_all = ydl.extract_info(url, download=False)
+
+    if data_all is None:
+        media = Media(url=url, status=Status.ERROR_URL, downloader=False)
+        media_out = media
+    else:
+        if "entries" in data_all:
+            media = Media(url=data_all["id"],
+                                title=data_all["title"], downloader=False)
+            medialist = data_all["entries"]
+            media_list = [Media(url=m["id"], title=m["title"]) for m in medialist if not m is None]
+            if not self.media_list:
+                media.status = Status.ERROR_URL
+                media_out = media
+            else:
+                media.count = len(data_all["entries"])
+                media_out = (media, media_list)
+        else:
+            media = Media(url=data_all["id"], title=data_all["title"])
+            media_out = media
+    return media_out
 
 class Format:
     AUDIO = "audio"
     VIDEO = "video"
     BOTH = "audio/video"
-
-
-class Type:
-    PLAYLIST = 1
-    SINGLE = 0
 
 
 class Status:
@@ -24,78 +48,45 @@ class Status:
 
 class Media:
 
-    def __init__(self, url='', title='', status=Status.OK, idx=0):
+    def __init__(self, url='', title='', status=Status.OK, downloader=True):
         self.url = url
         self.title = title
         self.status = status
-        self.idx = idx
-
-
-class Downloader:
-
-    def __init__(self, url):
-        self._error = False
-        self._flag_download = th.Event()
-        with yt.YoutubeDL({'ignoreerrors': True}) as ytd:
-            data_all = ytd.extract_info(url, download=False)
-
-        if data_all is None:
-            self.media = Media(url=url, status=Status.ERROR_URL)
-            self.type = Type.SINGLE
-        else:
-            self.media = Media(url=data_all["id"],
-                               title=data_all["title"])
-            if "entries" in data_all:
-                medialist = data_all["entries"]
-                self.count = 0
-                self.type = Type.PLAYLIST
-                self.playlist_idx = []
-                self.media_list = []
-                self.media.status = Status.OK
-                for media in medialist:
-                    if not media is None:
-                        self.media_list.append(
-                            Media(url=media["id"],
-                                  title=media["title"],
-                                  idx=self.count))
-                        self.playlist_idx.append(self.count)
-                    self.count += 1
-                if not self.media_list:
-                    self.type = Type.SINGLE
-                    self.media.status = Status.ERROR_URL
-            else:
-                self.type = Type.SINGLE
-                self.media.status = Status.OK
-
-    def start(self, **kwargs):
-        opts = {
-            'format': 'bestaudio/best',
-            'quiet': True,
-            'progress_hooks': [self.hook],
-            'ignoreerrors': True,
-        }
-        out = kwargs['dest'] + "%(id)s"
-        if kwargs['format'] == Format.AUDIO:
-            out += ".mp3"
-            opts['postprocessors'] = [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-            }]
-        if kwargs['format'] == Format.VIDEO:
-            out += ".mp4"
-        opts['outtmpl'] = out
-        with yt.YoutubeDL(opts) as ydl:
-            ydl.download([self.media.url])
-
+        self.format = None
+        self._downloadable = False
+        if downloader:
+            self._flag_download = th.Event()
+            self._downloadable = True
+            opts = {
+                'format': 'bestaudio/best',
+                'quiet': True,
+                'ignoreerrors': True,
+                'progress_hooks': [self.hook]
+            }
+            self._ydl = yt.YoutubeDL(opts)
+        
+    def start_download(self, dest):
+        if self._downloadable:
+            self._ydl.params['outtmpl'] = dest + "%(id)s"
+            if self.format == Format.AUDIO:
+                self._ydl.params['postprocessors'] = [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3'
+                }]
+                self._ydl.params['outtmpl'] += ".mp3"
+            if self.format == Format.VIDEO:
+                self._ydl.params['outtmpl'] += ".mp4"
+            self._ydl.download([self.url])
+    
     def wait_download(self):
         self._flag_download.wait()
 
-    def hook(self, d):
+    def hook(self, dl_data):
         if d['status'] == 'finished':
-            self.media.status = Status.DONE
+            self.status = Status.DONE
             os.rename(d['filename'], d['filename'].replace(
-                self.media.url, self.media.title))
+                self.url, self.title))
             self._flag_download.set()
         if d['status'] == 'error':
-            self.media.status = Status.ERROR_DOWNLOAD
+            self.status = Status.ERROR_DOWNLOAD
             self._flag_download.set()
